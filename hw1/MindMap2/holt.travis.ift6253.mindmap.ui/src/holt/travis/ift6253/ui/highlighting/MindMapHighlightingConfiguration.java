c3a7d9bb88bdcb480b71a073391a8a9def72c3fb
travis holt <trvslhlt@gmail.com>
2026-09-17 16:52:20 -0400

    add styling for topic references and priorities

--- hw1/MindMap2/holt.travis.ift6253.mindmap.ui/src/holt/travis/ift6253/ui/highlighting/MindMapHighlightingConfiguration.java
+++ hw1/MindMap2/holt.travis.ift6253.mindmap.ui/src/holt/travis/ift6253/ui/highlighting/MindMapHighlightingConfiguration.java
@@ -1,5 +1,6 @@
 package holt.travis.ift6253.ui.highlighting;
 
+import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
@@ -9,17 +10,35 @@ import org.eclipse.xtext.ui.editor.utils.TextStyle;
 public class MindMapHighlightingConfiguration extends DefaultHighlightingConfiguration {
 
 	public static final String TAG_ID = "tag";
+	public static final String RELATED_TOPIC_ID = "relatedTopic";
+	public static final String PRIORITY_ID = "priority";
 
 	@Override
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		super.configure(acceptor);
 		acceptor.acceptDefaultHighlighting(TAG_ID, "Tag", tagTextStyle());
+		acceptor.acceptDefaultHighlighting(RELATED_TOPIC_ID, "Related Topic Reference", relatedTopicTextStyle());
+		acceptor.acceptDefaultHighlighting(PRIORITY_ID, "Priority", priorityTextStyle());
 	}
 
 	private TextStyle tagTextStyle() {
 		TextStyle style = defaultTextStyle().copy();
 		style.setColor(new RGB(255, 165, 0));
 		style.setStyle(SWT.ITALIC);
+		return style;
+	}
+
+	private TextStyle relatedTopicTextStyle() {
+		TextStyle style = defaultTextStyle().copy();
+		style.setColor(new RGB(100, 100, 255));
+		style.setStyle(TextAttribute.UNDERLINE);
+		return style;
+	}
+
+	private TextStyle priorityTextStyle() {
+		TextStyle style = defaultTextStyle().copy();
+		style.setColor(new RGB(0, 255, 0));
+		style.setStyle(SWT.BOLD);
 		return style;
 	}
 }
--- hw1/MindMap2/holt.travis.ift6253.mindmap.ui/src/holt/travis/ift6253/ui/highlighting/MindMapSemanticHighlightingCalculator.java
+++ hw1/MindMap2/holt.travis.ift6253.mindmap.ui/src/holt/travis/ift6253/ui/highlighting/MindMapSemanticHighlightingCalculator.java
@@ -10,9 +10,9 @@ import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.CancelIndicator;
 
-import holt.travis.ift6253.mindMap.MindMap;
-import holt.travis.ift6253.mindMap.MindMapPackage;
+import holt.travis.ift6253.mindMap.Tag;
 import holt.travis.ift6253.mindMap.Topic;
+import holt.travis.ift6253.mindMap.MindMapPackage;
 
 public class MindMapSemanticHighlightingCalculator extends DefaultSemanticHighlightingCalculator {
 
@@ -24,17 +24,20 @@ public class MindMapSemanticHighlightingCalculator extends DefaultSemanticHighli
 		TreeIterator<EObject> contents = resource.getAllContents();
 		while (contents.hasNext()) {
 			EObject content = contents.next();
-			if (content instanceof MindMap) {
-				highlightFeature(content, MindMapPackage.Literals.MIND_MAP__TAGS, acceptor);
+			if (content instanceof Tag) {
+				// only the tag's value, so the 'tag' keyword keeps default style
+				highlightFeature(content, MindMapPackage.Literals.TAG__NAME, MindMapHighlightingConfiguration.TAG_ID, acceptor);
 			} else if (content instanceof Topic) {
-				highlightFeature(content, MindMapPackage.Literals.TOPIC__TAGS, acceptor);
+				highlightFeature(content, MindMapPackage.Literals.TOPIC__TAGS, MindMapHighlightingConfiguration.TAG_ID, acceptor);
+				highlightFeature(content, MindMapPackage.Literals.TOPIC__RELATED, MindMapHighlightingConfiguration.RELATED_TOPIC_ID, acceptor);
+				highlightFeature(content, MindMapPackage.Literals.TOPIC__PRIORITY, MindMapHighlightingConfiguration.PRIORITY_ID, acceptor);
 			}
 		}
 	}
 
-	private void highlightFeature(EObject object, EStructuralFeature feature, IHighlightedPositionAcceptor acceptor) {
+	private void highlightFeature(EObject object, EStructuralFeature feature, String highlightingId, IHighlightedPositionAcceptor acceptor) {
 		for (INode node : NodeModelUtils.findNodesForFeature(object, feature)) {
-			acceptor.addPosition(node.getOffset(), node.getLength(), MindMapHighlightingConfiguration.TAG_ID);
+			acceptor.addPosition(node.getOffset(), node.getLength(), highlightingId);
 		}
 	}
 }
