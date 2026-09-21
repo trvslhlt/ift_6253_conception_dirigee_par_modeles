be27f740014489d93dbd606178124899cc5b9c54
travis holt <trvslhlt@gmail.com>
2026-09-21 12:17:13 -0400

    wip samples

--- hw1/samples/courseProject00.mm
+++ hw1/samples/courseProject00.mm
@@ -1,6 +1,20 @@
 mindmap CourseProject {
-	- preliminary_research (H) {
-		
+	- phases {
+		- preparation (H) {
+			- find_candidate_topics
+		}
+		- execution
+		- delivery
 	}
-	
+	- about {
+		-> definition {
+			description: "https://www.iro.umontreal.ca/~syriani/courses/IFT6253-2026A/Project/projet.html"
+		}
+		-> schedule {
+			- milestone_1
+			- milestone_2
+			- submission
+			- presentation
+		}
+	}
 }
